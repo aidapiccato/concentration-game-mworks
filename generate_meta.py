4d98257ddcb22690cbe6b83c9789cac2910aefb3
Aida Piccato <aidapiccato@dhcp-18-21-133-34.dyn.mit.edu>
2020-01-21 14:28:05 -0500

    converting to ios

--- generate_meta.py
+++ generate_meta.py
@@ -1,6 +1,7 @@
 import pickle as pk
-from utils import MAX_N_PAIRS, MAX_N_IMAGES, N_CONFIG_REPEATS, ALL_N_PAIRS, N_CYCLES, CONFIGS, dir_path
 import numpy as np
+from constants import *
+
 
 def generate_subject_meta(subject_id):
     """
@@ -13,26 +14,40 @@ def generate_subject_meta(subject_id):
     for n_pairs in ALL_N_PAIRS:
         for _ in range(N_CONFIG_REPEATS):
             grid = np.repeat(np.arange(n_pairs), 2)
+            if np.isin(n_pairs, [4, 12]):  # will have one 'oddball' card
+                grid = np.append(grid, n_pairs)  # adding oddball index
+
             np.random.shuffle(grid)
 
             img_ixs = 2 * np.repeat(np.random.choice(np.arange(MAX_N_PAIRS), size=(n_pairs, 1), replace=False), 2) \
-                      + np.tile([0, 1], n_pairs)  ## randomly choose n_pair images
+                      + np.tile([0, 1], n_pairs)  # randomly choose n_pair images
 
-            inv_grid = np.zeros(MAX_N_IMAGES) - 1
+            if np.isin(n_pairs, [4, 12]):
+                oddball = np.random.choice(np.setdiff1d(np.arange(MAX_N_PAIRS), img_ixs)) # randomly choose an image
+                img_ixs = np.append(img_ixs, oddball)                                     # that is not already a pair
+            inv_grid = np.zeros(MAX_N_IMAGES) - 1                                         # to be oddball
             inv_grid[img_ixs] = np.argsort(grid)
             inv_grid = np.asarray(inv_grid, dtype=int)
 
-            trials = np.random.permutation(np.repeat(np.arange(n_pairs * 2), N_CYCLES))
+            if np.isin(n_pairs, [4, 12]):
+                oddball = inv_grid[img_ixs[-1]]
+                choices = np.setdiff1d(np.arange(n_pairs * 2 + 1), [oddball])
+            else:
+                choices = np.arange(n_pairs * 2)
+
+            trials = np.random.permutation(np.repeat(choices, N_CYCLES))
 
             config = {'n_pairs': n_pairs, 'grid_dims': CONFIGS[n_pairs], 'grid': grid,
-                      'inv_grid': inv_grid, 'feedback': 0, 'trials': trials}
+                      'inv_grid': inv_grid, 'trials': trials, 'img_ixs': img_ixs}
 
             meta.append(config)
 
     meta = np.asarray(meta)
 
-    np.random.shuffle(meta)
+    # repeatedly shuffling array until no consecutive blocks have the same grid size
+    while np.any([meta[i]['n_pairs'] == meta[i - 1]['n_pairs'] or meta[i]['n_pairs'] == meta[i + 1]['n_pairs']
+                  for i in range(1, len(meta) - 1)]):
+        np.random.shuffle(meta)
 
     with open(subject_fn, 'wb') as f:
         pk.dump(meta, f)
-
--- utils.py
+++ utils.py
@@ -1,26 +1,14 @@
 import numpy as np
 import pickle as pk
 
-MAX_N_IMAGES = 36
-MAX_N_PAIRS = 18
-
-CONFIGS = { # mapping number of pairs to possible configurations
-    2: [2, 2],
-    8: [4, 4],
-    18: [6, 6]
-}
-ALL_N_PAIRS = [2, 8, 18] # possible number of pairs in a configuration
-N_CONFIG_REPEATS = 4 # number of times each configuration is repeated (each time with unique image layout)
-N_CYCLES = 3
-
 username = 'aidapiccato'
 # username = 'apiccato'
 
-dir_path = '/Users/%s/PyCharmProjects/concentration/concentration-game-mworks' % username
-
+# dir_path = '/Users/%s/PyCharmProjects/concentration/concentration-game-mworks' % username
+dir_path = '~/Documents/MWorks/Data'
 subject_id = getvar('subject_id')
 
-subject_fn = '%s/meta/subject_%s.pkl' % (dir_path, subject_id)
+subject_fn = '%s/subject_%s.pkl' % (dir_path, subject_id)
 
 with open(subject_fn, 'rb') as f:
     subject_meta = pk.load(f)
@@ -30,7 +18,6 @@ def get_block_metaparameters():
     config = subject_meta[block_index]
     setvar('py_n_pairs', config['n_pairs'])
     setvar('py_grid_dims', np.asarray(config['grid_dims'], dtype=int))
-    setvar('py_feedback', int(config['feedback']))
     setvar('py_grid', np.asarray(config['grid'], dtype=int))
     setvar('py_inv_grid', np.asarray(config['inv_grid'], dtype=int))
 
